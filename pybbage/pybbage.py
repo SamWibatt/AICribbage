857c1fa88b994354aade355ef05465ebbb5f5f12
Sean Igo <samwibatt@gmail.com>
2020-03-18 11:17:26 -0700

    scoring 15s

--- pybbage/pybbage.py
+++ pybbage/pybbage.py
@@ -78,6 +78,16 @@ def srandom(seed):
 
 # card handling routines -------------------------------------------------------------------------
 
+def rank(card):
+    return card // 4
+
+def val(card):
+    '''value, s.t. ace = 1, 2 = 2, ... 10 and face cards = 10'''
+    return rank(card)+1 if rank(card) < 10 else 10
+
+def suit(card):
+    return card % 4
+
 
 def cardstring(card):
     '''card is a number from 0-51; card %4 is rank, where 0 = hearts, 1 = diamonds, 2 = clubs, 3 = spades.
@@ -86,7 +96,7 @@ def cardstring(card):
     if card not in range(0,52):
         print("Illegal card value",card)
         return None
-    return 'A234567890JQK'[card // 4] + '♥♦♣♠'[card % 4]
+    return 'A234567890JQK'[rank(card)] + '♥♦♣♠'[suit(card)]
 
 # shuffle returns a data structure containing parallel lists of card value (rank/suit combo 0..51) and flag
 # for whether it's been dealt.
@@ -124,6 +134,16 @@ def deal_card(deck,cardnum):
 # Cut will take an index into a deck which is assumed not to have any cards removed from it, plus an index.
 # then it swaps the halves. returns the cut deck.
 # weirdness is that cut (deck,0) returns deck unchanged. so, disallow 0?
+# ektully from sec 3.1 of the rules,
+# When cutting for the first deal of a game, the
+# first player shall remove no less than four cards
+# and not more than half the pack. The second
+# player shall remove no less than four cards and
+# shall leave at least four cards.
+# c. When cutting before each deal and for the starter
+# card, no less than four cards shall be taken from
+# the top and no less than four left on the bottom.
+# TODO encode all this!!!!!!!!!!!!!!!!!!!!!!!!!
 def cut(deck,index):
     if index >= 1 and index < len(deck):
         return deck[index:] + deck[:index]
@@ -156,18 +176,104 @@ def cut(deck,index):
 # so that they don't get restored e.g. between the play/countl and the scoring
 # any reason not to just use a list and numcards kind of arrangement?
 # fixed memory is helpful on tiny machines, just not pythonic
-class Hand:
-    def __init__(self):
-        pass
+# This cards and flags thing seems super clumsy
+# may resurrect for doing hypothetical hands, but currently just overthinking
+# let's just try without it
+# class Hand:
+#     # constants for card flags
+#     # Held means it is currently in hand and playable
+#     # Discarded means given to crib (should it even still be in the hand?)
+#     # in_play means it's not discarded but has been played in the play/count
+#     HELD = 0
+#     DISCARDED = 1
+#     IN_PLAY = 2
+#
+#
+#     # ctor
+#     def __init__(self, cards = None, flags = None):
+#         # I can imagine that we may need to spin up "hypothetical" hands directly without calling add_card
+#         self.cards = cards
+#         self.flags = flags
+#         pass
+#
+#     # add_card assumes the card is legit (0..51, non-duplicate) and it's not in play or otherwise
+#     # unusual (flag=0) though you can override the flag.
+#     # returns the index of the new card
+#     def add_card(self,card,flag=HELD):
+#         if self.cards is None:
+#             self.cards = []
+#             self.flags = []
+#         self.cards.append(card)
+#         self.flags.append(flag)
+#         return len(cards)-1
+#
+#     def get_card(self,i):
+#         if self.cards is None or len(self.cards) <=i:
+#             return None
+#         return self.cards[i]
+#
+#     def get_flag(self,i):
+#         if self.flags is None or len(self.flags) <=i:
+#             return None
+#         return self.flags[i]
 
 # K so I have a player object. What do we know about players?
-# - what cards they hold and which are "used"?
+# - what cards they hold and which are "used"
 # - whether they're the dealer
 # - their score
 class Player:
     def __init__(self):
+        self.cards = []
+        self.used_cards = []
         pass
 
+    def add_card(self,card):
+        self.cards.append(card)
+
+
+# let's just try scoring a show, damn the data structures for now
+# where hand is a list of 0..51, starter is the up-card 0..51
+# assumed to be correct i.e. no duplicates or illegal values
+def score_shew(hand,starter):
+    curscore = 0
+
+    # for convenience
+    cards = hand + [starter]
+
+    # look for fifteens ===============================================================================================
+    # what is the power set of cards? I suppose we need to check
+    # all two-card sums (of which there are 5 choose 2 = 10)
+    for i in range(0,4):
+        for j in range(i+1,5):
+            if val(cards[i]) + val(cards[j]) == 15:
+                curscore += 2
+                print(cardstring(cards[i]),cardstring(cards[j]),"... 15 -",curscore)
+    # all three-card sums (of which there are 5 choose 3 = 10, which = inverse of 5 choose 2, I guess)
+    for i in range(0,3):
+        for j in range(i+1,4):
+            for k in range(j+1,5):
+                if val(cards[i]) + val(cards[j]) + val(cards[k]) == 15:
+                    curscore += 2
+                    print(cardstring(cards[i]), cardstring(cards[j]), cardstring(cards[k]), "... 15 -", curscore)
+    # all four-card sums (of which there are 5 choose 4 = 5)
+    for i in range(0,5):
+        subcards = [x for x in cards]           # need to make a new copy...??? yup
+        #print("About to remove",cards[i],"from",subcards)
+        subcards.remove(cards[i])
+        if sum([val(x) for x in subcards]) == 15:
+            curscore += 2
+            for cs in [cardstring(x) for x in subcards]:
+                print(cs,end=' ')
+            print("... 15 -",curscore)
+
+    # all 5 cards' sum (1 sum)
+    if sum([val(x) for x in cards]) == 15:
+        curscore += 2
+        for cs in [cardstring(x) for x in cards]:
+            print(cs, end=' ')
+        print("... 15 -", curscore)
+
+    # look for big stuff like double double runs ======================================================================
 
 
 
@@ -190,12 +296,31 @@ if __name__ == "__main__":
     # do a cut!
     # WRITE THIS!!!!!!!!!!!
     # then deal
-    for j in range(0,54):
-        (card, cardnum) = deal_card(deck, cardnum)
-        print(j,cardstring(card),cardnum)
+    #for j in range(0,54):
+    #    (card, cardnum) = deal_card(deck, cardnum)
+    #    print(j,cardstring(card),cardnum)
 
     # let's dump the new one as csv and see what's up - seems correct! sorting by order made the value column
     # consecutive. So away goes the auld way.
     #print("card,value,order")
     #for j in range(0,52):
     #    print("{},{},{}".format(cardstring(deck['value'][j]),deck['value'][j],deck['order'][j]))
+
+    # scoring!
+    hand = [19, 10, 42, 25]
+    starter = 18
+
+    print("Hand is",[cardstring(x) for x in hand])
+    print("Starter:",cardstring(starter))
+
+    score_shew(hand,starter)
+
+    print('---')
+
+    hand = [19, 10, 7, 25]
+    starter = 18
+
+    print("Hand is",[cardstring(x) for x in hand])
+    print("Starter:",cardstring(starter))
+
+    score_shew(hand,starter)
